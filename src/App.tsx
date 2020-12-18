594fc8d398894b718e21e523fc794fd2b91f9171
Artie Fischer <artie@curetechnologies.us>
2020-12-18 16:28:46 -0700

    wip: backtrack that!

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+
+    return (
+        <div>Hi There</div>
   );
 }
 
 export default App;
+
